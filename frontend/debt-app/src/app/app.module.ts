c2d9a3a2f9b3a323c83b5a4b0cb032ad5316cca5
Elkin Hurtado <syselkin5@gmail.com>
2021-04-20 23:01:18 -0500

    Agregar plantilla principal

--- frontend/debt-app/src/app/app.module.ts
+++ frontend/debt-app/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
+import { Nopagefound404Component } from './pages/nopagefound404/nopagefound404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    Nopagefound404Component,
+    DashboardComponent,
+    BreadcrumsComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

3d997ccc431682f4a79a95f346564b9641deb366
Elkin Hurtado <syselkin5@gmail.com>
2021-05-08 00:55:06 -0500

    Agregar documentacion y prueba con swagger

---
+++ backend/debt-app-customer-api/src/main/java/org/kinsoft/debt_app/customer_api/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package org.kinsoft.debt_app.customer_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("org.kinsoft.debt_app.customer_api"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(getApiInfo());
+  }
+
+  private ApiInfo getApiInfo() {
+    return new ApiInfo(
+        "Customer API",
+        "Customer of the app debt",
+        "1.0",
+        "http://codmind.com/terms",
+        new Contact("kingsoft", "https://kingsoft.com", "syselkin5@gmail.com"),
+        "LICENSE",
+        "LICENSE URL",
+        Collections.emptyList());
+  }
+}
